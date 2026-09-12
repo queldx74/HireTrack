7b4e4f8de124fd7b139c716882ef784531a570d8
queldx74 <xar_anris@hotmail.com>
2026-09-12 19:13:26 +0100

    configure Django deployment settings

--- hiretrack/settings.py
+++ hiretrack/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['127.0.0.1',
-    'localhost', 'hiretrackerheroku.herokuapp.com',]
+    'localhost', 'hiretracker.herokuapp.com',]
 
 
 # Application definition
