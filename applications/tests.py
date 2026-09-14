5f40fb0d6f3be7c430e4ef4ce9ff1d001de293af
queldx74 <xar_anris@hotmail.com>
2026-09-14 19:23:33 +0100

    Add automated tests for job applications

--- applications/tests.py
+++ applications/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import JobApplication
 
-# Create your tests here.
+
+class JobApplicationModelTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testuser",
+            password="testpassword123",
+        )
+
+        self.application = JobApplication.objects.create(
+            user=self.user,
+            job_title="Software Developer",
+            company="Test Company",
+            date_applied="2026-09-14",
+            status="applied",
+        )
+
+    def test_application_created(self):
+        self.assertEqual(
+            self.application.company,
+            "Test Company"
+        )
+
+    def test_application_belongs_to_user(self):
+        self.assertEqual(
+            self.application.user,
+            self.user
+        )
